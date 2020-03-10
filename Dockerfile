894cebc420a9d1546a1aeb803bd3fd182671f099
Tsan-Kuang Lee <tsan.kuang.lee@gmail.com>
2020-03-10 00:19:11 -0500

    new way to compile

--- Dockerfile
+++ Dockerfile
@@ -4,13 +4,4 @@ VOLUME /qmk_firmware
 WORKDIR /qmk_firmware
 COPY . .
 
-ENV keyboard=ergodox
-ENV subproject=ez
-ENV keymap=default
-
-VOLUME /qmk
-WORKDIR /qmk
-CMD groupadd -g ${GROUPID} user; useradd -g ${GROUPID} -u ${USERID} --no-create-home --home-dir /qmk user ; \
-    make clean ; make keyboard=${keyboard} subproject=${subproject} keymap=${keymap} ; \
-    chown -R user:user .build *.hex ;
 CMD make all:default
--- compile.sh
+++ compile.sh
@@ -1,28 +1,33 @@
 #!/bin/sh
 
-export QMK_DOCKER_IMAGE="qmk-compiler"
-
-if [[ "$(docker images -q ${QMK_DOCKER_IMAGE} 2> /dev/null)" == "" ]]; then
-    docker build --rm -t ${QMK_DOCKER_IMAGE} .
-fi
+#export QMK_DOCKER_IMAGE="qmk-compiler"
+#
+#if [[ "$(docker images -q ${QMK_DOCKER_IMAGE} 2> /dev/null)" == "" ]]; then
+#    docker build --rm -t ${QMK_DOCKER_IMAGE} .
+#fi
 
 export KEYBOARD="ergodox_ez"
 export KEYMAP="tsankuanglee"
 export HEXNAME=${KEYBOARD}_${KEYMAP}.hex
 
 # compile
-docker run -it \
-    -e USERID=`id -u` -e GROUPID=`id -g` \
-    -e keymap=$KEYMAP -e keyboard=$KEYBOARD --rm -v $('pwd'):/qmk:rw ${QMK_DOCKER_IMAGE}
-
-# clean up; no need to keep it in the root
-rm -f ./${HEXNAME}
-
-# flashing
-../teensy_loader_cli.git/teensy_loader_cli -mmcu=atmega32u4 -w -v .build/${HEXNAME}
-
-# copy the files to archived
-export TIMESTAMP=`date +%Y%m%d-%H%M%S`
-export KEYMAP_FOLDER=./keyboards/${KEYBOARD}/keymaps/${KEYMAP}
-rsync -at ${KEYMAP_FOLDER}/keymap.c ${KEYMAP_FOLDER}/archived/${TIMESTAMP}-keymap.c
-rsync -at .build/${HEXNAME} ${KEYMAP_FOLDER}/archived/${TIMESTAMP}-${HEXNAME}
+# old version
+#docker run -it \
+#    -e USERID=`id -u` -e GROUPID=`id -g` \
+#    -e keymap=$KEYMAP -e keyboard=$KEYBOARD --rm -v $('pwd'):/qmk:rw ${QMK_DOCKER_IMAGE}
+# new version
+docker-compose run --rm\
+    compiler \
+    bash -c "qmk compile -kb ${KEYBOARD} -km ${KEYMAP}; qmk flash -kb ${KEYBOARD} -km  ${KEYMAP}"
+#
+## clean up; no need to keep it in the root
+#rm -f ./${HEXNAME}
+#
+## flashing
+#../teensy_loader_cli.git/teensy_loader_cli -mmcu=atmega32u4 -w -v .build/${HEXNAME}
+#
+## copy the files to archived
+#export TIMESTAMP=`date +%Y%m%d-%H%M%S`
+#export KEYMAP_FOLDER=./keyboards/${KEYBOARD}/keymaps/${KEYMAP}
+#rsync -at ${KEYMAP_FOLDER}/keymap.c ${KEYMAP_FOLDER}/archived/${TIMESTAMP}-keymap.c
+#rsync -at .build/${HEXNAME} ${KEYMAP_FOLDER}/archived/${TIMESTAMP}-${HEXNAME}
